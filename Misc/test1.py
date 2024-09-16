f9741a5308e04be4d3428a53bc09e6296bcc7e81
Ansaf Nagori <71640162+kenichi-07@users.noreply.github.com>
2024-09-16 14:09:30 -0500

    Delete Misc directory

--- Misc/test1.py
+++
@@ -1,6 +0,0 @@
-status_check()
-{
-x=`aws opensearch describe-domain-config --domain-name pvai-product-demo1 --profile product --region us-east-1 --no-verify-ssl --query 'DomainConfig.EngineVersion.Status.State'`
-}
-
-status_check
